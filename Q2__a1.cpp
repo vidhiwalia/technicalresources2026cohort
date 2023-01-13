29035d228befa85657abf3a760727e0ae5a63077
vidhiwalia <122613807+vidhiwalia@users.noreply.github.com>
2023-01-13 23:02:24 +0530

    submitting ques 2 of assignment 1

---
+++ Q2__a1.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int marks;
+    cout<<"enter the marks of student";
+    cin>>marks;
+    if marks>90{
+        cout<<"excellent";
+    }
+    elif marks>80&&marks<=90{
+        cout<<"good";
+    }
+    elif marks>70&&marks<=80;{
+        cout<<"fair";
+        
+    }
+    elif marks>60&&marks<=70{
+        cout<<"fair";
+        
+    }
+    else{
+        cout<<"below par";
+        
+    }
+return 0;
+}

55706e52faddcdff1d0d3ed2610f4856355c6c12
vidhiwalia <122613807+vidhiwalia@users.noreply.github.com>
2023-01-13 22:22:33 +0530

    submitting question 1 of assignment 1

---
+++ q1__a1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{//printing the z pattern
+    cout<<"*****"<<endl;
+    cout<<"   *"<<endl;
+    cout<<"  *"<<endl;
+    cout<<" *"<<endl;
+    cout<<"*****"<<endl;
+return 0;
+}

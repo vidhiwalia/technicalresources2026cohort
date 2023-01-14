f902a560d60582ec9b4319fcad3311b926761be7
vidhiwalia <122613807+vidhiwalia@users.noreply.github.com>
2023-01-14 13:45:23 +0530

    submitting ques 3 of assignment 1

---
+++ Q3__a1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num;
+    for(int j=2;j<num;j++){
+        cout<<"enter the number pls";
+        cin>>num;
+
+        if(num%j==0){
+            cout<<"not prime"<<endl;
+                
+        }
+        else{
+            cout<<"prime"<<endl;
+        }
+
+            
+    }
+return 0;
+}
